05a5a5a663f11b988c2c1d4c518fac787f9acb12
zredcay <60413606+zredcay@users.noreply.github.com>
2023-01-08 15:52:44 +0000

    switched to check every hour

--- main.py
+++ main.py
@@ -9,18 +9,17 @@ from keep_alive import keep_alive
 from get_prompt import get_prompt
 
 
-def round_time(dt=None, round_to=60):
-    """Round a datetime object to any time-lapse in seconds
+def round_time(dt=None, round_to=3600):
+  """Round a datetime object to any time-lapse in seconds
    dt : datetime.datetime object, default now.
    roundTo : Closest number of seconds to round to, default 1 minute.
    Author: Thierry Husson 2012 - Use it as you want but don't blame me.
    """
-    if dt is None:
-        dt = datetime.datetime.now()
-    seconds = (dt.replace(tzinfo=None) - dt.min).seconds
-    rounding = (seconds + round_to / 2) // round_to * round_to
-    return dt + datetime.timedelta(0, rounding - seconds, -dt.microsecond)
-
+  if dt is None:
+    dt = datetime.datetime.now()
+  seconds = (dt.replace(tzinfo=None) - dt.min).seconds
+  rounding = (seconds + round_to / 2) // round_to * round_to
+  return dt + datetime.timedelta(0, rounding - seconds, -dt.microsecond)
 
 
 load_dotenv()
@@ -44,7 +43,7 @@ async def send_daily_message():
 
     # Get a time object for the current hour and minute
     # Replace this with your desired time
-    msg_time = datetime.time(hour=12, minute=0)
+    msg_time = datetime.time(hour=12, minute=00)
 
     # Combine the date and time objects into a single datetime object
     message_time = datetime.datetime.combine(today, msg_time)
@@ -60,20 +59,27 @@ async def send_daily_message():
         # Fetch the user by their user id
         user = await client.fetch_user(user_id)
         # Send the message to the user
-        await user.send("Prompts are empty, prolly should update that bro. Also send it manually today or not up to you lel.")
+        await user.send(
+          "Prompts are empty, prolly should update that bro. Also send it manually today or not up to you lel."
+        )
       else:
         # Replace "channel_id" with the ID of the channel you want to send the message to
         channel = await client.fetch_channel(channel_id)
-        await channel.send(f"@everyone Today's drawing challenge is to draw Spook or one of the other voidlings as {prompt}. Good luck")
+        await channel.send(
+          f"@everyone Today's drawing challenge is to draw Spook or one of the other voidlings as {prompt}. Good luck"
+        )
 
-    await asyncio.sleep(60)  # Check the current time every minute
+    await asyncio.sleep(3600)  # Check the current time every hour
 
 
 async def main():
-  await client.login(TOKEN)
+  keep_alive()
+  try:
+    await client.login(TOKEN)
+  except: 
+    os.system("kill 1")
   print("Discord client started")
   await send_daily_message()
 
 
-keep_alive()
 asyncio.run(main())
