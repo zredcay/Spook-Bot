090b737b11e87ea1477767812b716028580548d4
zredcay <60413606+zredcay@users.noreply.github.com>
2023-01-24 20:14:23 +0000

    added locked variable to prevent double sends in one day, resets at midnight

--- main.py
+++ main.py
@@ -32,7 +32,9 @@ client = discord.Client(intents=discord.Intents.all())
 
 
 async def send_daily_message():
+  locked = False
   while not client.is_closed():
+    # Get a time object for the current hour and minute
     current_time = datetime.datetime.utcnow()
 
     # Round the current time to the nearest minute
@@ -41,18 +43,27 @@ async def send_daily_message():
     # Get a date object for today's date
     today = datetime.date.today()
 
-    # Get a time object for the current hour and minute
     # Replace this with your desired time
     msg_time = datetime.time(hour=12, minute=00)
 
     # Combine the date and time objects into a single datetime object
     message_time = datetime.datetime.combine(today, msg_time)
 
+    # Replace this with your desired rest time
+    rst_time = datetime.time(hour=7, minute=00)
+
+    # Combine the date and time objects into a single datetime object
+    reset_time = datetime.datetime.combine(today, rst_time)
+
+    print(current_time)
     print(rounded_time)
     print(message_time)
+    print(reset_time)
     print(rounded_time == message_time)
+    print(rounded_time == reset_time)
 
-    if rounded_time == message_time:
+    if rounded_time == message_time and not locked:
+      locked = True
       prompt = get_prompt().rstrip()
 
       if prompt == "end":
@@ -69,6 +80,8 @@ async def send_daily_message():
           f"@everyone Today's drawing challenge is to draw Spook or one of the other voidlings as {prompt}. Good luck"
         )
 
+    if rounded_time == reset_time and locked:
+      locked = False
     await asyncio.sleep(3600)  # Check the current time every hour
 
 
